3b2365fff78ccf0efc7bddfc2f22787e0c81d082
janekhq <janekzagorski@proton.me>
2026-09-17 19:44:48 +0200

    fix: socket cors fix

--- app/sockets/instance.py
+++ app/sockets/instance.py
@@ -1,3 +1,3 @@
 import socketio
 
-sio = socketio.AsyncServer(async_mode='asgi', cors_allowed_origins=['http://localhost:3000', 'https://cams.varely.co'], cors_credentials=True)
+sio = socketio.AsyncServer(async_mode='asgi', cors_allowed_origins=['http://localhost:3000', 'https://eye.varely.co'], cors_credentials=True)
